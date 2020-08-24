f6516c1e896ea41816d8bedbc55bc888ffd63552
ZooH <zero5.two4@gmail.com>
2020-08-25 05:49:40 +0900

    1.4.1 Make a To To List

--- greeting.js
+++ greeting.js
@@ -1,40 +1,38 @@
 const form = document.querySelector(".js-form"),
-    input = form.querySelector("input"),
-    greeting = document.querySelector(".js-greetings");
+  input = form.querySelector("input"),
+  greeting = document.querySelector(".js-greeting");
 
 const USER_LS = "currentUser",
-    SHOWING_CN = "showing";
+  SHOWING_CN = "showing";
 
 function saveName(text){
     localStorage.setItem(USER_LS, text);
 }
-
-function handleSubmit(event){
+function handleSubmit(){
     event.preventDefault();
     const currentValue = input.value;
-    paintGreeting(currentValue);
     saveName(currentValue);
+    paintGreeting(currentValue);
 }
 function askForName(){
     form.classList.add(SHOWING_CN);
     form.addEventListener("submit", handleSubmit);
 }
-function paintGreeting(text){
-    form.classList.remove(SHOWING_CN);
-    greeting.classList.add(SHOWING_CN);
-    greeting.innerHTML = `Hello ${text}`;
+function paintGreeting(text) {
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerHTML = `Hello ${text}!`;
 }
-
-function loadName(){
-    const currentUser = localStorage.getItem(USER_LS);
-    if (currentUser === null){
-        askForName();
-    }else{
-        paintGreeting(currentUser);
-    }
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+      askForName();
+  } else {
+    paintGreeting(currentUser);
+  }
 }
-function init(){
-    loadName();
+function init() {
+  loadName();
 }
 
 init();
