49f8a1b7357cc6419b5c5124f6c41146ba5ec60c
Ju Hyeok <40654227+zero5two4@users.noreply.github.com>
2020-08-25 16:44:47 +0900

    1.5.1 Put Background Image

---
+++ bg.js
@@ -0,0 +1,20 @@
+const body = document.querySelector("body");
+
+const IMG_NUMBER = 1;
+
+function paintImage(imgNumber){
+    const image = new Image();
+    image.src = `./images/${imgNumber}.jpg`;
+    image.classList.add("bgImage");
+    body.prepend(image);
+}
+function getRandom(){
+    const number = Math.floor(Math.random() * IMG_NUMBER);
+    return number;
+}
+function init(){
+    const randomNumber = getRandom();
+    paintImage(randomNumber);
+}
+
+init();
